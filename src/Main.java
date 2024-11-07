88472e3aeb07889132c4dd9d9050b664340737b8
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2024-11-07 13:02:36 +0000

    exercicio

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ class Main { // escopo {}
     System.out.println("pronto");
     // array de inteiros
     int[] idades = {12, 76, 6, 87, 32};
+
+    // TODO: implementar amplitude
+    int amplitude = Estatistica.amplitude(idades);
+    // 81
+    System.out.println(amplitude); // 81
     
     try {
       int m = Estatistica.max(idades);
@@ -24,10 +29,18 @@ class Main { // escopo {}
     
     // mediana
     // {4, 5, 8, 10, 12} = 8
-    // {4, 5, 8, 10, 12, 14} = 9
+    
     int[] valores = {8, 10, 12, 4, 5};
     int mediana = Estatistica.mediana(valores);
     System.out.println(mediana); // 8
+
+    // {4, 5, 8, 10, 12, 14} = 9
+    int[] valores2 = {4, 5, 8, 10, 12, 14};
+    int mediana2 = Estatistica.mediana(valores2);
+    // FIXME: fazer retornar 9
+    System.out.println(mediana2); // deve dar 9
+
+
   }
   
   
